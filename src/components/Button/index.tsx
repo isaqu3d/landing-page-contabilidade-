90f9638bcf2f2c257c858afabf770d21f8b0ecd1
Isaque de Sousa <isaque.error404@gmail.com>
2026-02-04 15:41:24 -0300

    feat: add outline prop to Button component for enhanced styling options

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -5,18 +5,20 @@ import * as S from './styles';
 
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   variant?: 'primary' | 'secondary';
+  outline?: boolean;
   fullWidth?: boolean;
   children: React.ReactNode;
 }
 
 export function Button({
   variant = 'secondary',
+  outline = false,
   fullWidth = false,
   children,
   ...props
 }: ButtonProps) {
   return (
-    <S.StyledButton $variant={variant} $fullWidth={fullWidth} {...props}>
+    <S.StyledButton $variant={variant} $outline={outline} $fullWidth={fullWidth} {...props}>
       {children}
     </S.StyledButton>
   );
