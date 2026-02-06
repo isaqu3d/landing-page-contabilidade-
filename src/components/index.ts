36d7f06b21032a12ac394c032339e4ababfb3faa
Isaque de Sousa <isaque.error404@gmail.com>
2026-02-06 17:09:37 -0300

    feat: export Footer component from index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { Button } from './Button';
+export { Footer } from './Footer';
 export { Header } from './Header';
 export { HeroSection } from './HeroSection';
 export { Typography } from './Typography';
